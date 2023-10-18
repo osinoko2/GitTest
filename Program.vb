ac4f986638aab3f995c45e4a79ade682ecafe7ae
osinoko <k023g0114@g.neec.ac.jp>
2023-10-18 14:25:17 +0900

    a

--- Program.vb
+++ Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello GitHub!")
         Console.WriteLine("2023/10/11")
+        Console.WriteLine("マージしてください！")
     End Sub
 End Module
